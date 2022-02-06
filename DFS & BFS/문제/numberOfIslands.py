f1abaf3eba3556d41b3d4570958a35f4562609a0
leesaeyoon <ericalee97@naver.com>
2022-02-06 14:58:02 +0900

    dfs

---
+++ DFS & BFS/문제/numberOfIslands.py
@@ -0,0 +1,76 @@
+## numberOfIslands
+"""
+link: https://leetcode.com/problems/number-of-islands/
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water),
+return the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+"""
+
+# 아이디어
+# DFS / BFS
+
+# 주의할 점
+# 이미 방문한 곳은 표시
+
+from typing import *
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+            grid[i][j] = '#'
+            dfs(i-1, j)
+            dfs(i, j-1)
+            dfs(i+1, j)
+            dfs(i, j+1)
+
+        result = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    result += 1
+
+        return result
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.numIslands([
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"]
+    ])) # 1
+    print(sol.numIslands([
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"]
+    ])) # 3
